8a305c7a79ac5a512edd7c6682f65ea6f8053996
aHasnat1997 <hasnat56703@gmail.com>
2023-05-14 03:40:39 +0600

    appointments post api done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xclxsua.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +29,7 @@ async function run() {
     await client.connect();
 
     const servicesCollection = client.db("carDoctorDB").collection("services");
+    const appointmentsCollection = client.db("carDoctorDB").collection("appointments");
 
     app.get('/services', async (req, res) => {
       const options = {
@@ -37,7 +39,25 @@ async function run() {
       };
       const result = await servicesCollection.find().toArray();
       res.send(result);
-    })
+    });
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: {
+          _id: 1, title: 1, img: 1, price: 1, service_id: 0, description: 0, facility: 0
+        },
+      };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/appointments', async (req, res) => {
+      const doc = req.body;
+      const result = await appointmentsCollection.insertOne(doc);
+      res.send(result);
+    });
 
 
     // Send a ping to confirm a successful connection
